29a5f4255e7544d4217c1abbe4b5dfbb51368be6
Jack Burnett <jackjburnett@gmail.com>
2025-02-09 20:44:19 +0000

    Improved robustness of GamepadManager

--- GamepadManager.py
+++ GamepadManager.py
@@ -3,32 +3,46 @@ import vgamepad as vg
 # Dictionary to store gamepad objects for each value
 gamepads = {}
 
+# Gamepad types
+XBOX_GAMEPAD = vg.VX360Gamepad
+DS4_GAMEPAD = vg.VDS4Gamepad
 
-# Function for getting/creating a gamepad based on name (IP Address)
+
 def get_or_create_gamepad(name):
-    # If the gamepad already exists for this IP, return it
-    if name in gamepads:
-        return gamepads[name]
-    else:
-        # Otherwise, create a new gamepad and store it in the dictionary
-        gamepad = vg.VX360Gamepad()
-        gamepads[name] = gamepad
-        print(f"Created new gamepad for {name}")
-        return gamepad
+    """
+    Get or create a gamepad based on the given name (IP Address).
+
+    Args:
+        name (str): The name (IP Address) of the gamepad.
+
+    Returns:
+        vg.VX360Gamepad: The gamepad object.
+    """
+    try:
+        if name in gamepads:
+            return gamepads[name]
+        else:
+            gamepad = XBOX_GAMEPAD()
+            gamepads[name] = gamepad
+            print(f"Created new gamepad for {name}")
+            return gamepad
+    except Exception as e:
+        print(f"Error creating gamepad: {e}")
+        return None
 
 
-# Function for safely deleting gamepads
 def delete_gamepad(name):
-    # If the gamepad exists for the given IP, release all controls and remove it
+    """
+    Safely delete a gamepad based on the given name (IP Address).
+
+    Args:
+        name (str): The name (IP Address) of the gamepad.
+    """
     if name in gamepads:
         print(f"Deleting gamepad for {name}")
         gamepad = gamepads[name]
-
-        # Optionally: Reset the gamepad (release buttons, reset sticks, etc.)
         gamepad.reset()
         gamepad.update()
-
-        # Remove the gamepad from the dictionary
         del gamepads[name]
     else:
         print(f"No gamepad found for {name}")
